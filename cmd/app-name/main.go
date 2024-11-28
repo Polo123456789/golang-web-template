84cc13d779e0d1921052025678b0fbf5a4c860d3
Polo123456789 <pabsan02@gmail.com>
2024-11-28 14:54:56 -0600

    Primer Commit

---
+++ cmd/app-name/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"github.com/Polo123456789/assert"
+
+	"github.com/Polo123456789/golang-web-template/internal/http"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt, os.Kill,
+	)
+
+	assert.Enabled = true
+
+	logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+
+	server := http.NewServer(
+		"0.0.0.0",
+		8080,
+		logger,
+	)
+
+	http.RunServer(ctx, cancel, server, logger)
+}
